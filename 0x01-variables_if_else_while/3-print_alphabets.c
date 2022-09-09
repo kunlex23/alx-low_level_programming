db45ddbba3311ee2c52710ba1871db20c35183a7
kunlex23.com <kunlex23@gmail.com>
2022-09-09 16:00:23 -0700

    varriable commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char 1;
+
+l ='a';
+while(l <='z')
+{
+putchar(1);
+l++;
+}
+l = 'A';
+while (l <= 'Z')
+{
+putchar(l);
+l++;
+}
+putchar('\n');
+return (0);
+}
