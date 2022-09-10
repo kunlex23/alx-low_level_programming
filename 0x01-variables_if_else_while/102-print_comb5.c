d6ab729a6b2f6f5a9a178f2e3103455319eac30c
kunlex23.com <kunlex23@gmail.com>
2022-09-09 18:23:50 -0700

    varriable commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,8 +20,7 @@ putchar(j);
 putchar(32);
 putchar(k);
 putchar(l);
-if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
-{
+if ((i == 57) && (j == 56) && (k == 57) && (l == 57)){
 putchar('\n');
 i++;
 }
