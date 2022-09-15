6d8b9f39ff196921f859cca683af723393af40a7
kunlex23.com <kunlex23@gmail.com>
2022-09-15 14:36:33 -0700

    Debug push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ for (i = 1; i <= 100; i++)
 if (i == 100)
 printf("%s", b);
 else if ((i % 3 == 0) && (i % 5 == 0))
-rintf("%s ", fb);
+printf("%s ", fb);
 else if (i % 3 == 0)
 printf("%s ", f);
 else if (i % 5 == 0)
