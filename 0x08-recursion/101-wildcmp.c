4643fd2fb8d2ab4b465542d13cd95d734fc7389b
kunlex23.com <kunlex23@gmail.com>
2022-09-28 00:44:19 -0700

    reccursion pushing

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -40,7 +40,6 @@ return (0);
  */
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
-
 if (s1[a] != '\0')
 {
 if (s2[b] == '\0')
@@ -58,16 +57,14 @@ return (checker(s1, s2, a + 1, b + 1, b));
 }
 else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
 return (checker(s1, s2, a + 1, b + 1, wildUsed));
-
 if (wildUsed == -1)
 return (0);
-
 return (checker(s1, s2, a, wildUsed, wildUsed));
-
 }
 if (s2[b] != '\0')
 return (checkLast(s2, b));
 
 return (1);
+
 }
 
