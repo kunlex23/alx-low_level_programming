853b42b3388a87aed7fa5f4a0d6f392ca2927e65
kunlex23.com <kunlex23@gmail.com>
2022-10-05 16:43:16 -0700

    Debug push

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,14 +13,11 @@ char *_strdup(char *str)
 {
 int i = 0, size = 0;
 char *m;
-
 if (str == NULL)
 return (NULL);
-for (; str[size] != '\0'; size++;
-
+for (; str[size] != '\0'; size++);
 /*+1 on the size puts the end of string character*/
 m = malloc(size * sizeof(*str) + 1);
-
 if (m == 0)
 return (NULL);
 else
@@ -30,4 +27,3 @@ m[i] = str[i];
 }
 return (m);
 }
-
