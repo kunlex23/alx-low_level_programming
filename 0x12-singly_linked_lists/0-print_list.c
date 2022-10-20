d66808d65cb238905fdc9638f8b6420b078ffdd8
kunlex23.com <kunlex23@gmail.com>
2022-10-20 14:06:46 -0700

    dingly_linked_list push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,24 +8,25 @@
  */
 size_t print_list(const list_t *h)
 {
-int count = 0;
+	int count = 0;
 
-if (h == NULL)
-return (0);
+	if (h == NULL)
+		return (0);
 
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[%d] %s\n", 0, "(nil)");
-}
-else
-{
-printf("[%d] %s\n", h->len, h->str);
-}
-h = h->next;
-count++;
-}
-return (count);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+	{
+		printf("[%d] %s\n", 0, "(nil)");
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	h = h->next;
+	count++;
+	}
+
+	return (count);
 }
 
